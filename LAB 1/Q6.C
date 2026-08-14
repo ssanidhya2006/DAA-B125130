1177189f550262a879d05e793c9d576cf4adf4b3
ssanidhya2006 <s.sanidhya2006@gmail.com>
2026-08-14 15:54:17 +0530

    LAB 1

--- LAB 1/Q6.C
+++ LAB 1/Q6.C
@@ -1,33 +1,40 @@
-#include<stdio.h>
-int main(){
-    int n,i,j;
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
     int a[100];
-    int dup=0;
+    int dup = 0;
 
-    printf("enter no of elements:");
-    scanf("%d",&n);
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
 
-    printf("enter elements:\n");
-    for(i=0;i<n;i++){
-        scanf("%d",&a[i]);
+    printf("Enter the elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
     }
 
-    for(i=0;i<n;i++){
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(a[i]==a[j]){
-                dup=1;
+    // Check for duplicate elements
+    for(i = 0; i < n - 1; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[i] == a[j])
+            {
+                dup = 1;
+                break;
             }
         }
+
+        if(dup == 1)
+            break;
     }
-    
-}
 
-if(dup==1)
-  printf("duplicate elements found");
-else
-  printf("all elements are unique");
-  
-  return 0;
+    if(dup == 1)
+        printf("Duplicate elements found.");
+    else
+        printf("All elements are unique.");
 
+    return 0;
 }
